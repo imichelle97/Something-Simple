c6bd0e9c2913dad57828e802f0b4d5762d9f02ca
Michelle <michelle.luong2015@gmail.com>
2018-11-12 22:42:24 -0800

    new data dump, including contact table

--- OFS.sql
+++ OFS.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Nov 12, 2018 at 08:54 AM
+-- Generation Time: Nov 13, 2018 at 07:41 AM
 -- Server version: 10.1.34-MariaDB
 -- PHP Version: 5.6.37
 
@@ -21,6 +21,25 @@ SET time_zone = "+00:00";
 --
 -- Database: `OFS`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_contact`
+--
+
+CREATE TABLE `admin_contact` (
+  `contact_id` int(11) NOT NULL,
+  `name` varchar(256) NOT NULL,
+  `message` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin_contact`
+--
+
+INSERT INTO `admin_contact` (`contact_id`, `name`, `message`) VALUES
+(8, 'Michelle Luong', 'test');
 
 -- --------------------------------------------------------
 
@@ -119,58 +138,58 @@ CREATE TABLE `item` (
 --
 
 INSERT INTO `item` (`item_id`, `item_category`, `item_name`, `item_desc`, `item_price`, `item_weight`, `item_weight_unit`, `inventory`, `image`) VALUES
-(1, 'Produce', 'Bananas', 'min 5 ct.', '1.55', '1.70', 'lbs', 50, 'images/OrganicBananas.jpg'),
-(2, 'Produce', 'Baby Carrots', 'Grown in the United States ', '1.69', '1.00', 'lbs', 50, 'images/OrganicBabyCarrots.jpg'),
-(3, 'Produce', 'Strawberries', 'Grown in the United States or Mexico', '4.49', '1.00', 'lbs', 50, 'images/OrganicStrawberries.jpg'),
-(4, 'Produce', 'Gala Apple', 'Grown in Washington or Chile', '4.89', '3.00', 'lbs', 50, 'images/OrganicGalaApple.jpg'),
-(5, 'Produce', 'Blueberries', 'Grown in United States, Mexico, Peru, Chile, Argentina and Canada', '3.99', '0.38', 'lbs', 50, 'images/OrganicBlueberries.jpg'),
-(6, 'Produce', 'Baby Spinich', 'Triple-washed', '4.99', '1.00', 'lbs', 50, 'images/OrganicBabySpinach.jpg'),
-(7, 'Produce', 'Raspberries', 'Grown in the United States, Mexico, or Chile', '4.99', '0.38', 'lbs', 50, 'images/OrganicRaspberries.jpg'),
-(8, 'Produce', 'Gold Potatoes', NULL, '5.99', '5.00', 'lbs', 50, 'images/OrganicGoldPotatoes.jpg'),
-(9, 'Produce', 'Yellow Onion', NULL, '4.99', '3.00', 'lbs', 0, 'images/YellowOnionOrganic.jpg'),
-(10, 'Produce', 'White Mushrooms', NULL, '3.49', '0.62', 'lbs', 0, 'images/OrganicWhiteMushrooms.jpg'),
-(11, 'Produce', 'Whole Milk', 'Vitamin D', '5.49', '8.00', 'lbs', 50, 'images/OrganicWholeMilk.jpg'),
-(12, 'Produce', 'Zucchini Squash', 'Grown in United States or Mexico', '3.00', '1.50', 'lbs', 0, 'images/OrganicZucchiniSquash.jpg'),
-(13, 'Breads and Bakery', 'Bread', '21 Whole Grains', '4.99', '1.69', 'lbs', 0, 'images/21WholeGrains.jpg'),
-(14, 'Breads and Bakery', 'Bread', 'White Bread', '5.99', '1.50', 'lbs', 0, 'images/WhiteBread.jpg'),
-(15, 'Breads and Bakery', 'Bread', 'Whole Wheat', '5.99', '1.56', 'lbs', 0, 'images/WholeWheat.jpg'),
-(16, 'Breads and Bakery', 'Bread', 'Sourdough Bread', '4.99', '1.50', 'lbs', 0, 'images/SourdoughBread.jpg'),
+(1, 'Produce', 'Organic Bananas (min 5 ct.)', '', '1.55', '1.70', 'lbs', 50, 'images/OrganicBananas.jpg'),
+(2, 'Produce', 'Organic Baby Carrots', '- Grown in the United States ', '1.69', '1.00', 'lbs', 47, 'images/OrganicBabyCarrots.jpg'),
+(3, 'Produce', 'Organic Strawberries', '- Grown in the United States or Mexico', '4.49', '1.00', 'lbs', 49, 'images/OrganicStrawberries.jpg'),
+(4, 'Produce', 'Organic Gala Apple', '- Grown in Washington or Chile', '4.89', '3.00', 'lbs', 50, 'images/OrganicGalaApple.jpg'),
+(5, 'Produce', 'Organic Blueberries', '- Grown in United States, Mexico, Peru, Chile, Argentina and Canada', '3.99', '0.38', 'lbs', 50, 'images/OrganicBlueberries.jpg'),
+(6, 'Produce', 'Organic Baby Spinich', '- Triple-washed', '4.99', '1.00', 'lbs', 50, 'images/OrganicBabySpinach.jpg'),
+(7, 'Produce', 'Organic Raspberries', '- Grown in the United States, Mexico, or Chile', '4.99', '0.38', 'lbs', 48, 'images/OrganicRaspberries.jpg'),
+(8, 'Produce', 'Organic Gold Potatoes', NULL, '5.99', '5.00', 'lbs', 48, 'images/OrganicGoldPotatoes.jpg'),
+(9, 'Produce', 'Yellow Onion Organic', NULL, '4.99', '3.00', 'lbs', 47, 'images/YellowOnionOrganic.jpg'),
+(10, 'Produce', 'Organic White Mushrooms', NULL, '3.49', '0.62', 'lbs', 0, 'images/OrganicWhiteMushrooms.jpg'),
+(11, 'Produce', 'Organic Whole Milk', '- Vitamin D', '5.49', '8.00', 'lbs', 50, 'images/OrganicWholeMilk.jpg'),
+(12, 'Produce', 'Organic Zucchini Squash', '- Grown in United States or Mexico', '3.00', '1.50', 'lbs', 0, 'images/OrganicZucchiniSquash.jpg'),
+(13, 'Breads and Bakery', 'Organic Bread', '21 Whole Grains', '4.99', '1.69', 'lbs', 0, 'images/21WholeGrains.jpg'),
+(14, 'Breads and Bakery', 'Organic Bread', 'White Bread', '5.99', '1.50', 'lbs', 0, 'images/WhiteBread.jpg'),
+(15, 'Breads and Bakery', 'Organic Bread', 'Whole Wheat', '5.99', '1.56', 'lbs', 0, 'images/WholeWheat.jpg'),
+(16, 'Breads and Bakery', 'Organic Bread', 'Sourdough Bread', '4.99', '1.50', 'lbs', 0, 'images/SourdoughBread.jpg'),
 (17, 'Breads and Bakery', 'English Muffins', 'Whole Grain', '0.37', '0.06', 'lbs', 0, 'images/EnglishMuffins.jpg'),
-(18, 'Dairy, Cheese, and Eggs', 'Large Brown Eggs', ' Free-Range Brown Eggs', '5.79', '1.50', 'lbs', 0, 'images/LargeBrownEggs.jpg'),
+(18, 'Dairy, Cheese, and Eggs', 'Large Brown Eggs', 'Organic Free-Range Brown Eggs', '5.79', '1.50', 'lbs', 0, 'images/LargeBrownEggs.jpg'),
 (19, 'Dairy, Cheese, and Eggs', 'Whole Milk', 'Organic', '5.49', '8.00', 'lbs', 0, 'images/WholeMilk.jpg'),
-(20, 'Dairy, Cheese, and Eggs', 'Clarified Butter Oil', 'Ghee', '9.36', '0.81', 'lbs', 0, 'images/ClarifiedButterOil.jpg'),
+(20, 'Dairy, Cheese, and Eggs', 'Clarified Butter Oil', 'Organic Ghee', '9.36', '0.81', 'lbs', 0, 'images/ClarifiedButterOil.jpg'),
 (21, 'Dairy, Cheese, and Eggs', 'Heavy Whipping Cream', 'Ultra Pasteurized', '4.29', '1.00', 'lbs', 0, 'images/HeavyWhippingCream.jpg'),
 (22, 'Dairy, Cheese, and Eggs', 'String Cheese', 'Mozzarella', '4.99', '0.38', 'lbs', 0, 'images/StringCheese.jpg'),
-(23, 'Dairy, Cheese, and Eggs', 'Butter', 'Salted', '4.79', '1.00', 'lbs', 0, 'images/Butter.jpg'),
-(24, 'Deli', 'Pepperoni', 'Uncured Slices', '5.50', '0.31', 'lbs', 0, 'images/OrganicPepperoni.jpg'),
-(25, 'Deli', 'Ham', 'Uncured Black Forest Ham', '7.99', '0.38', 'lbs', 0, 'images/OrganicHam.jpg'),
-(26, 'Deli', 'Chicken Breast', 'Oven Roasted', '7.99', '0.38', 'lbs', 0, 'images/OrganicChickenBreast.jpg'),
-(27, 'Deli', 'Beef Hot Dogs', 'Uncured 100% Grassfed', '7.66', '0.75', 'lbs', 0, 'images/BeefHotDogs.jpg'),
-(28, 'Deli', 'Smoked Turkey Breast', 'Sliced Smoked Turkey Breast', '6.64', '0.38', 'lbs', 0, 'images/SmokedTurkeyBreast.jpg'),
-(29, 'Deli', 'Slow Cooked Ham', 'Uncured', '5.89', '0.38', 'lbs', 0, 'images/SlowCookedHam.jpg'),
-(30, 'Deli', 'Genoa Salami', 'Uncured', '6.99', '0.25', 'lbs', 0, 'images/GenoaSalami.jpg'),
+(23, 'Dairy, Cheese, and Eggs', 'Butter', 'Organic, Salted', '4.79', '1.00', 'lbs', 0, 'images/Butter.jpg'),
+(24, 'Deli', 'Organic Pepperoni', 'Uncured Slices', '5.50', '0.31', 'lbs', 0, 'images/OrganicPepperoni.jpg'),
+(25, 'Deli', 'Organic Ham', 'Uncured Black Forest Ham', '7.99', '0.38', 'lbs', 0, 'images/OrganicHam.jpg'),
+(26, 'Deli', 'Organic Chicken Breast', 'Oven Roasted', '7.99', '0.38', 'lbs', 0, 'images/OrganicChickenBreast.jpg'),
+(27, 'Deli', 'Beef Hot Dogs', 'Organic Uncured 100% Grassfed', '7.66', '0.75', 'lbs', 0, 'images/BeefHotDogs.jpg'),
+(28, 'Deli', 'Smoked Turkey Breast', 'Organic Sliced Smoked Turkey Breast', '6.64', '0.38', 'lbs', 0, 'images/SmokedTurkeyBreast.jpg'),
+(29, 'Deli', 'Slow Cooked Ham', 'Organic Uncured', '5.89', '0.38', 'lbs', 0, 'images/SlowCookedHam.jpg'),
+(30, 'Deli', 'Genoa Salami', 'Organic Uncured', '6.99', '0.25', 'lbs', 0, 'images/GenoaSalami.jpg'),
 (31, 'Frozen', 'Riced Cauliflower', 'Organic', '1.99', '0.75', 'lbs', 0, 'images/RicedCauliflower.jpg'),
 (32, 'Frozen', 'Sweet Yellow Corn', 'Organic', '1.99', '1.00', 'lbs', 0, 'images/SweetYellowCorn.jpg'),
 (33, 'Frozen', 'Butternut Squash', 'Organic', '2.99', '1.00', 'lbs', 0, 'images/ButternutSquash.jpg'),
 (34, 'Frozen', 'Mixed Vegetables', 'Organic, Unsalted', '2.29', '1.00', 'lbs', 0, 'images/MixedVegetables.jpg'),
-(35, 'Frozen', 'Brown Rice', 'Whole Grain', '2.99', '1.25', 'lbs', 0, 'images/BrownRice.jpg'),
-(36, 'Frozen', 'Three Pepper Blend', 'Frozen', '2.99', '1.00', 'lbs', 0, 'images/ThreePepperBlend.jpg'),
-(37, 'Frozen', 'Green Peas', 'Unsalted', '2.29', '1.00', 'lbs', 0, 'images/GreenPeas.jpg'),
-(38, 'Frozen', 'Blueberries', 'Frozen', '8.99', '2.00', 'lbs', 0, 'images/Blueberries.jpg'),
-(39, 'Frozen', 'Macaroni & Cheese', 'Frozen', '3.79', '0.56', 'lbs', 0, 'images/Macaroni&Cheese.jpg'),
+(35, 'Frozen', 'Brown Rice', 'Organic Whole Grain', '2.99', '1.25', 'lbs', 0, 'images/BrownRice.jpg'),
+(36, 'Frozen', 'Three Pepper Blend', 'Organic, Frozen', '2.99', '1.00', 'lbs', 0, 'images/ThreePepperBlend.jpg'),
+(37, 'Frozen', 'Green Peas', 'Organic, Unsalted', '2.29', '1.00', 'lbs', 0, 'images/GreenPeas.jpg'),
+(38, 'Frozen', 'Blueberries', 'Organic, Frozen', '8.99', '2.00', 'lbs', 0, 'images/Blueberries.jpg'),
+(39, 'Frozen', 'Macaroni & Cheese', 'Organic, Frozen', '3.79', '0.56', 'lbs', 0, 'images/Macaroni&Cheese.jpg'),
 (40, 'Frozen', 'White Rice', 'Thai Jasmine Rice', '2.99', '1.25', 'lbs', 0, 'images/WhiteRice.jpg'),
 (41, 'Frozen', 'Mixed Mushrooms', 'Organic', '3.99', '0.62', 'lbs', 0, 'images/MixedMushrooms.jpg'),
 (42, 'Meat and Seafood', 'Chicken Breast', 'Boneless & Skinless', '10.26', '1.00', 'lbs', 0, 'images/ChickenBreast.jpg'),
 (43, 'Meat and Seafood', 'Ground Chicken', 'Organic', '5.55', '1.00', 'lbs', 0, 'images/GroundChicken.jpg'),
-(44, 'Meat and Seafood', 'Whole Chicken', 'Whole Chicken with Giblets, Free Range, All Natural', '13.76', '5.00', 'lbs', 0, 'images/WholeChicken.jpg'),
-(45, 'Meat and Seafood', 'Sunday Bacon', 'Uncured', '7.99', '0.50', 'lbs', 0, 'images/SundayBacon.jpg'),
+(44, 'Meat and Seafood', 'Whole Chicken', 'Organic Whole Chicken with Giblets, Free Range, All Natural', '13.76', '5.00', 'lbs', 0, 'images/WholeChicken.jpg'),
+(45, 'Meat and Seafood', 'Sunday Bacon', 'Organic Uncured', '7.99', '0.50', 'lbs', 0, 'images/SundayBacon.jpg'),
 (46, 'Meat and Seafood', 'Stirloin Steaks', '100% Grass-Fed', '12.34', '0.62', 'lbs', 0, 'images/StirloinSteaks.jpg'),
 (47, 'Meat and Seafood', 'Pork Chops', 'Center Cut Boneless', '7.65', '0.50', 'lbs', 0, 'images/PorkChops.jpg'),
 (48, 'Meat and Seafood', 'Ground Beef', '100% Grass-Fed', '8.63', '1.00', 'lbs', 0, 'images/GroundBeef.jpg'),
-(49, 'Meat and Seafood', 'Stew Meat', '100% Grass-Fed', '13.55', '1.00', '', 0, 'images/StewMeat.jpg'),
-(50, 'Meat Substitutes', 'Tofu', 'Extra Firm', '2.49', '0.75', 'lbs', 0, 'images/Tofu.jpg'),
+(49, 'Meat and Seafood', 'Stew Meat', 'Organic, 100% Grass-Fed', '13.55', '1.00', '', 0, 'images/StewMeat.jpg'),
+(50, 'Meat Substitutes', 'Tofu', 'Organic Extra Firm', '2.49', '0.75', 'lbs', 0, 'images/Tofu.jpg'),
 (51, 'Meat Substitutes', 'Tempeh', 'Original Soy', '2.59', '0.50', 'lbs', 0, 'images/Tempeh.jpg'),
-(52, 'Meat Substitutes', 'Soyrizo', 'Soyrizo Meatless Soy Chorizo', '3.79', '0.75', 'lbs', 0, 'images/Soyrizo.jpg'),
+(52, 'Meat Substitutes', 'Soyrizo', 'Organic Soyrizo Meatless Soy Chorizo', '3.79', '0.75', 'lbs', 0, 'images/Soyrizo.jpg'),
 (53, 'Soups, Stocks, and Broths', 'Roasted Red Pepper & Tomato Soup', 'Organic', '3.99', '2.00', 'lbs', 0, 'images/RoastedRedPepper&TomatoSoup.jpg'),
 (54, 'Soups, Stocks, and Broths', 'Chicken Broth', 'Organic', '2.29', '2.00', 'lbs', 0, 'images/ChickenBroth.jpg'),
 (55, 'Soups, Stocks, and Broths', 'Chicken Stock', 'Organic', '2.99', '2.00', 'lbs', 0, 'images/ChickenStock.jpg'),
@@ -201,11 +220,24 @@ CREATE TABLE `orders` (
 INSERT INTO `orders` (`order_id`, `customer_id`, `username`, `item_id`, `quantity`) VALUES
 (1, 6, 'imichelle97', 1, 2),
 (2, 6, 'imichelle97', 3, 2),
-(3, 6, 'imichelle97', 4, 1);
+(3, 6, 'imichelle97', 4, 1),
+(4, 6, 'imichelle97', 8, 2),
+(5, 6, 'imichelle97', 7, 2),
+(6, 6, 'imichelle97', 2, 2),
+(7, 6, 'imichelle97', 9, 1),
+(8, 6, 'imichelle97', 9, 2),
+(9, 6, 'imichelle97', 2, 1),
+(10, 6, 'imichelle97', 3, 1);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admin_contact`
+--
+ALTER TABLE `admin_contact`
+  ADD PRIMARY KEY (`contact_id`);
 
 --
 -- Indexes for table `category`
@@ -243,6 +275,12 @@ ALTER TABLE `orders`
 --
 
 --
+-- AUTO_INCREMENT for table `admin_contact`
+--
+ALTER TABLE `admin_contact`
+  MODIFY `contact_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
 -- AUTO_INCREMENT for table `customer_profile`
 --
 ALTER TABLE `customer_profile`
@@ -258,7 +296,7 @@ ALTER TABLE `item`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Constraints for dumped tables
