a26817796465116de280a1db0dede16e757efeb4
Karl Lapuz <karllapuz25@gmail.com>
2018-11-15 17:03:38 -0800

    Change blurb and update database

--- OFS.sql
+++ OFS.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Nov 13, 2018 at 07:41 AM
+-- Generation Time: Nov 15, 2018 at 12:01 AM
 -- Server version: 10.1.34-MariaDB
 -- PHP Version: 5.6.37
 
@@ -33,13 +33,6 @@ CREATE TABLE `admin_contact` (
   `name` varchar(256) NOT NULL,
   `message` longtext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `admin_contact`
---
-
-INSERT INTO `admin_contact` (`contact_id`, `name`, `message`) VALUES
-(8, 'Michelle Luong', 'test');
 
 -- --------------------------------------------------------
 
@@ -72,6 +65,7 @@ INSERT INTO `category` (`category_name`) VALUES
 --
 
 CREATE TABLE `customer` (
+  `user_id` int(11) NOT NULL,
   `username` varchar(128) NOT NULL,
   `password` varchar(128) DEFAULT NULL,
   `email` varchar(128) DEFAULT NULL,
@@ -82,9 +76,11 @@ CREATE TABLE `customer` (
 -- Dumping data for table `customer`
 --
 
-INSERT INTO `customer` (`username`, `password`, `email`, `user_type`) VALUES
-('admin', 'dc76e9f0c0006e8f919e0c515c66dbba3982f785', 'admin@gmail.com', 'Admin'),
-('imichelle97', '9d4e1e23bd5b727046a9e3b4b7db57bd8d6ee684', 'imichelle97@gmail.com', 'user');
+INSERT INTO `customer` (`user_id`, `username`, `password`, `email`, `user_type`) VALUES
+(2, 'admin', 'dc76e9f0c0006e8f919e0c515c66dbba3982f785', 'admin@gmail.com', 'Admin'),
+(3, 'imichelle97', '9d4e1e23bd5b727046a9e3b4b7db57bd8d6ee684', 'imichelle97@gmail.com', 'user'),
+(4, 'cjavan', '9d4e1e23bd5b727046a9e3b4b7db57bd8d6ee684', 'cjavan@gmail.com', 'user'),
+(5, 'feathers', '9d4e1e23bd5b727046a9e3b4b7db57bd8d6ee684', 'test', 'user');
 
 -- --------------------------------------------------------
 
@@ -113,7 +109,7 @@ CREATE TABLE `customer_profile` (
 --
 
 INSERT INTO `customer_profile` (`customer_id`, `first_name`, `last_name`, `username`, `phone_number`, `address`, `city`, `state`, `zipcode`, `card_type`, `card_number`, `cvc`, `expiration_date`) VALUES
-(6, 'Michelle', 'Luong', 'imichelle97', '408-807-7560', '7190 Rosencrans Way', 'San Jose', 'CA', '95139', 'Visa', '1111-1111-1111-1111', '123', '2018-11-12');
+(11, 'Michelle', 'Luong', 'imichelle97', '408-807-7560', '5601 Great Oaks Pkwy', 'San Jose', 'CA', '95119', 'Discover', '8173-0917-2282-9167', '439', '2018-12');
 
 -- --------------------------------------------------------
 
@@ -138,17 +134,17 @@ CREATE TABLE `item` (
 --
 
 INSERT INTO `item` (`item_id`, `item_category`, `item_name`, `item_desc`, `item_price`, `item_weight`, `item_weight_unit`, `inventory`, `image`) VALUES
-(1, 'Produce', 'Organic Bananas (min 5 ct.)', '', '1.55', '1.70', 'lbs', 50, 'images/OrganicBananas.jpg'),
-(2, 'Produce', 'Organic Baby Carrots', '- Grown in the United States ', '1.69', '1.00', 'lbs', 47, 'images/OrganicBabyCarrots.jpg'),
-(3, 'Produce', 'Organic Strawberries', '- Grown in the United States or Mexico', '4.49', '1.00', 'lbs', 49, 'images/OrganicStrawberries.jpg'),
-(4, 'Produce', 'Organic Gala Apple', '- Grown in Washington or Chile', '4.89', '3.00', 'lbs', 50, 'images/OrganicGalaApple.jpg'),
+(1, 'Produce', 'Organic Bananas (min 5 ct.)', '', '1.55', '1.70', 'lbs', 47, 'images/OrganicBananas.jpg'),
+(2, 'Produce', 'Organic Baby Carrots', '- Grown in the United States ', '1.69', '1.00', 'lbs', 49, 'images/OrganicBabyCarrots.jpg'),
+(3, 'Produce', 'Organic Strawberries', '- Grown in the United States or Mexico', '4.49', '1.00', 'lbs', 50, 'images/OrganicStrawberries.jpg'),
+(4, 'Produce', 'Organic Gala Apple', '- Grown in Washington or Chile', '4.89', '3.00', 'lbs', 48, 'images/OrganicGalaApple.jpg'),
 (5, 'Produce', 'Organic Blueberries', '- Grown in United States, Mexico, Peru, Chile, Argentina and Canada', '3.99', '0.38', 'lbs', 50, 'images/OrganicBlueberries.jpg'),
 (6, 'Produce', 'Organic Baby Spinich', '- Triple-washed', '4.99', '1.00', 'lbs', 50, 'images/OrganicBabySpinach.jpg'),
-(7, 'Produce', 'Organic Raspberries', '- Grown in the United States, Mexico, or Chile', '4.99', '0.38', 'lbs', 48, 'images/OrganicRaspberries.jpg'),
-(8, 'Produce', 'Organic Gold Potatoes', NULL, '5.99', '5.00', 'lbs', 48, 'images/OrganicGoldPotatoes.jpg'),
-(9, 'Produce', 'Yellow Onion Organic', NULL, '4.99', '3.00', 'lbs', 47, 'images/YellowOnionOrganic.jpg'),
-(10, 'Produce', 'Organic White Mushrooms', NULL, '3.49', '0.62', 'lbs', 0, 'images/OrganicWhiteMushrooms.jpg'),
-(11, 'Produce', 'Organic Whole Milk', '- Vitamin D', '5.49', '8.00', 'lbs', 50, 'images/OrganicWholeMilk.jpg'),
+(7, 'Produce', 'Organic Raspberries', '- Grown in the United States, Mexico, or Chile', '4.99', '0.38', 'lbs', 49, 'images/OrganicRaspberries.jpg'),
+(8, 'Produce', 'Organic Gold Potatoes', NULL, '5.99', '5.00', 'lbs', 50, 'images/OrganicGoldPotatoes.jpg'),
+(9, 'Produce', 'Yellow Onion Organic', NULL, '4.99', '3.00', 'lbs', 50, 'images/YellowOnionOrganic.jpg'),
+(10, 'Produce', 'Organic White Mushrooms', NULL, '3.49', '0.62', 'lbs', 50, 'images/OrganicWhiteMushrooms.jpg'),
+(11, 'Produce', 'Organic Whole Milk', '- Vitamin D', '5.49', '8.00', 'lbs', 49, 'images/OrganicWholeMilk.jpg'),
 (12, 'Produce', 'Organic Zucchini Squash', '- Grown in United States or Mexico', '3.00', '1.50', 'lbs', 0, 'images/OrganicZucchiniSquash.jpg'),
 (13, 'Breads and Bakery', 'Organic Bread', '21 Whole Grains', '4.99', '1.69', 'lbs', 0, 'images/21WholeGrains.jpg'),
 (14, 'Breads and Bakery', 'Organic Bread', 'White Bread', '5.99', '1.50', 'lbs', 0, 'images/WhiteBread.jpg'),
@@ -218,16 +214,24 @@ CREATE TABLE `orders` (
 --
 
 INSERT INTO `orders` (`order_id`, `customer_id`, `username`, `item_id`, `quantity`) VALUES
-(1, 6, 'imichelle97', 1, 2),
-(2, 6, 'imichelle97', 3, 2),
-(3, 6, 'imichelle97', 4, 1),
-(4, 6, 'imichelle97', 8, 2),
-(5, 6, 'imichelle97', 7, 2),
-(6, 6, 'imichelle97', 2, 2),
-(7, 6, 'imichelle97', 9, 1),
-(8, 6, 'imichelle97', 9, 2),
-(9, 6, 'imichelle97', 2, 1),
-(10, 6, 'imichelle97', 3, 1);
+(17, 3, 'imichelle97', 1, 3),
+(18, 3, 'imichelle97', 1, 3),
+(19, 3, 'imichelle97', 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shipping`
+--
+
+CREATE TABLE `shipping` (
+  `id` int(11) NOT NULL,
+  `username` varchar(256) NOT NULL,
+  `address` varchar(256) NOT NULL,
+  `city` varchar(256) NOT NULL,
+  `state` varchar(256) NOT NULL,
+  `zipcode` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
@@ -249,7 +253,7 @@ ALTER TABLE `category`
 -- Indexes for table `customer`
 --
 ALTER TABLE `customer`
-  ADD PRIMARY KEY (`username`);
+  ADD PRIMARY KEY (`user_id`);
 
 --
 -- Indexes for table `customer_profile`
@@ -271,6 +275,12 @@ ALTER TABLE `orders`
   ADD PRIMARY KEY (`order_id`);
 
 --
+-- Indexes for table `shipping`
+--
+ALTER TABLE `shipping`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -281,10 +291,16 @@ ALTER TABLE `admin_contact`
   MODIFY `contact_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT for table `customer_profile`
 --
 ALTER TABLE `customer_profile`
-  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `item`
@@ -296,7 +312,13 @@ ALTER TABLE `item`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `shipping`
+--
+ALTER TABLE `shipping`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Constraints for dumped tables
